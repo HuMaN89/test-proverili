6ba9c5eb5b89821c026107cc14ba695f7c4dcdfd
HuMaN89 <Human_B_9@mail.ru>
2023-04-28 02:18:59 +0600

    feat: home work

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import "./index.css";
 
 const root = document.querySelector("#root");
-const cities = [
+const sities = [
   "Краснодар",
   "Екатеринбург",
   "Ростов-на-Дону",
@@ -12,6 +12,17 @@ const cities = [
   "Самара",
   "Пермь",
 ];
+
+const professions = [
+  "Любая",
+  "Экономика",
+  "Философия",
+  "Социология",
+  "Юриспруденция",
+  "Менеджмент",
+  "Екатеринбург",
+  "Краснодар",
+];
 const cashe = {
   step1: null,
   step2: "Москва",
@@ -19,20 +30,40 @@ const cashe = {
   step4: null,
   step5: null,
   step6: null,
-  step7: null,
+  step7: "Любая",
   step8: null,
-  step9: null,
+  step9: {
+    name: null,
+    phone: null,
+    email: null,
+  },
 };
 
 const quizContainer = document.querySelector(".quiz-container");
-const sitySelect = document.querySelector(".sity-select");
+
+const sitiesList = document.querySelector("#sities-list");
 const selectList = document.querySelector(".select-list");
 let activSity = "Москва";
 const sityName = document.querySelector(".sity-name");
+
+const profSelect = document.querySelector("#professions-select");
+const professionsList = document.querySelector("#professions-list");
+let avtivProf = "Любая";
+const profName = document.querySelector("#prof-name");
+
 const nextButtons = document.querySelectorAll(".btn");
 let offset = 0;
 const step1 = document.querySelector("#step1");
 const step2 = document.querySelector("#step2");
+const step3 = document.querySelector("#step3");
+const step4 = document.querySelector("#step4");
+const step5 = document.querySelector("#step5");
+const step6 = document.querySelector("#step6");
+const step7 = document.querySelector("#step7");
+const step8 = document.querySelector("#step8");
+const firstname = document.querySelector("#firstname");
+const phone = document.querySelector("#phone");
+const email = document.querySelector("#email");
 
 step1.addEventListener("click", (e) => {
   e.stopPropagation();
@@ -43,16 +74,51 @@ step1.addEventListener("click", (e) => {
 });
 step2.addEventListener("click", (e) => {
   e.stopPropagation();
-  console.log(e.target);
-  if (e.target.tagName === "LABEL") {
-    cashe.step1 = e.target.id;
-    nextButtons[0].disabled = false;
+  if (e.target.classList.contains("select-item")) {
+    cashe.step2 = e.target.innerHTML;
   }
 });
+step3.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target.tagName === "LABEL") {
+    cashe.step3 = e.target.id;
+    nextButtons[4].disabled = false;
+  }
+});
+step4.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target.tagName === "LABEL") {
+    cashe.step4 = e.target.id;
+    nextButtons[6].disabled = false;
+  }
+});
+step5.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target.tagName === "LABEL") {
+    cashe.step5 = e.target.id;
+    nextButtons[8].disabled = false;
+  }
+});
+step6.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target.tagName === "LABEL") {
+    cashe.step6 = e.target.id;
+    nextButtons[10].disabled = false;
+  }
+});
+step8.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target.tagName === "LABEL") {
+    cashe.step8 = e.target.id;
+    nextButtons[14].disabled = false;
+  }
+});
+
+// step 2
 const initSities = () => {
   sityName.innerHTML = activSity;
   selectList.innerHTML = `<div class="select-item active">${activSity}</div>`;
-  cities.forEach((sity) => {
+  sities.forEach((sity) => {
     sity !== activSity
       ? (selectList.innerHTML += `<div class="select-item">${sity}</div>`)
       : null;
@@ -60,10 +126,10 @@ const initSities = () => {
   initSitiesList();
 };
 const initSitiesList = () => {
+  console.log(sitiesList);
   selectList.childNodes.forEach((child) => {
     child.addEventListener("click", () => {
       activSity = child.innerHTML;
-      console.log(child.innerHTML);
       toggleElement(selectList);
       initSities();
     });
@@ -74,18 +140,20 @@ initSitiesList();
 const toggleElement = (element) => {
   element.classList.toggle("show");
 };
-sitySelect.addEventListener("click", () => {
+sitiesList.addEventListener("click", () => {
   toggleElement(selectList);
 });
 
 nextButtons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.value === "Далее") {
-      offset += -576;
+      offset += -562;
       quizContainer.style.transform = `translateX(${offset}px)`;
     } else {
-      offset += 576;
+      offset += 562;
       quizContainer.style.transform = `translateX(${offset}px)`;
     }
   });
 });
+
+//step 7
